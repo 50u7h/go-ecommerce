085b2afc4b9bdade91648e20d65242039eb51ff4
50u7h <50u7h@protonmail.com>
2023-10-01 18:24:31 +0300

    Setting an expiry for password reset emails

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -356,6 +356,13 @@ func (app *application) ShowResetPassword(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	// make sure not expired
+	expired := signer.Expired(testURL, 1)
+	if expired {
+		app.errorLog.Println("Link Expired")
+		return
+	}
+
 	data := make(map[string]interface{})
 	data["email"] = r.URL.Query().Get("email")
 
