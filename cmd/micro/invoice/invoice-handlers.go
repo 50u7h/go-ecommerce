1eab2ccc01ad54eff4e1ec3d5678192edec5472b
50u7h <50u7h@protonmail.com>
2023-10-04 22:34:41 +0300

    Receiving data with micrsoservice

--- cmd/micro/invoice/invoice-handlers.go
+++ cmd/micro/invoice/invoice-handlers.go
@@ -3,8 +3,44 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
+// Order describes the json payload received by this microservice
+type Order struct {
+	ID        int       `json:"id"`
+	Quantity  int       `json:"quantity"`
+	Amount    int       `json:"amount"`
+	Product   string    `json:"product"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 func (app *application) CreateAndSendInvoice(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello, %s", "world")
+	// receive json
+	var order Order
+
+	err := app.readJSON(w, r, &order)
+	if err != nil {
+		err := app.badRequest(w, r, err)
+		if err != nil {
+			return
+		}
+		return
+	}
+
+	// send reponse
+	var resp struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+	resp.Error = false
+	resp.Message = fmt.Sprintf("Invoice %d.pdf created and sent to %s", order.ID, order.Email)
+
+	err = app.writeJSON(w, http.StatusCreated, resp)
+	if err != nil {
+		return
+	}
 }
